c00e876b1c28f08903e1a9ff45ff1f925eb68630
Nikozszs <06@yandex.ru>
2025-11-13 16:54:23 +0300

    add /auth/me req

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bcrypt from 'bcrypt'
 import { validationResult } from 'express-validator';
 import { registerValidation } from './validations/auth.js'
 import UserModel from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
 
 mongoose.connect('mongodb+srv://igormelnikov94_db_user:B3CClaZFwDYeXJMi@cluster0.7mmqj3e.mongodb.net/blog?appName=Cluster0',   
 )
@@ -15,8 +16,61 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) =>{
-    res.send('111Hello')
+app.get('/auth/me', checkAuth, async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId)
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Пользователь не найден'
+            })
+        }
+
+        const { passwordHash, ...userData } = user._doc
+
+        res.json(userData)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Нет доступа"
+        })
+    }
+})
+
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email })
+        if (!user) {
+            return res.status(404).json({
+                message: 'Пользователь не найден',
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Неверный логин или пароль',
+            })
+        }
+
+        const token = jwt.sign({
+            _id: user._id.toString()
+            }, 
+                'secret123',
+            {
+                expiresIn: '30d'
+        })
+
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({...userData, token})
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Не удалось авторизоваться"
+        })
+    }
 })
 
 app.post('/auth/register', registerValidation, async (req, res) => {
@@ -41,9 +95,9 @@ app.post('/auth/register', registerValidation, async (req, res) => {
 
         const token = jwt.sign({
             _id: user._id
-        }, 
-        'secret123',
-        {
+            }, 
+            'secret123',
+            {
             expiresIn: '30d'
         })
 
