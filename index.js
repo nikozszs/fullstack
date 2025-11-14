c9ae4f4272d1e9f193ae3dc8623b8e1fc652721e
Nikozszs <06@yandex.ru>
2025-11-14 16:13:09 +0300

    add rout posts(delete, get, post, patch)

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import multer from 'multer'
 import mongoose from 'mongoose'
-import { registerValidation } from './validations/auth.js'
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js'
 import checkAuth from './utils/checkAuth.js'
-import { register, login, getMe} from './controllers/UserController.js'
+import { register, login, getMe } from './controllers/UserController.js'
+import { create, getAll, getOne, remove, update } from './controllers/PostController.js'
+
 
 mongoose.connect('mongodb+srv://igormelnikov94_db_user:B3CClaZFwDYeXJMi@cluster0.7mmqj3e.mongodb.net/blog?appName=Cluster0',   
 )
@@ -15,9 +18,15 @@ app.use(express.json())
 
 app.get('/auth/me', checkAuth, getMe)
 
-app.post('/auth/login', login)
+app.post('/auth/login', loginValidation, login)
 
 app.post('/auth/register', registerValidation, register)
+
+app.get('/posts', getAll)
+app.get('/post/:id', getOne)
+app.post('/posts', checkAuth, postCreateValidation, create)
+app.delete('/posts/:id', checkAuth, remove)
+app.patch('/posts/:id', checkAuth, update)
 
 app.listen(4444, (err) =>{
     if (err) {
---
+++ models/Post.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const PostSchema = new mongoose.Schema({
+        title: {
+            type: String,
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        tags: {
+            type: Array,
+            default: []
+        },
+        imageUrl: String,
+        viewsCount: {
+            type: Number,
+            default: 0
+        }
+    }, {
+        timestamps: true,
+    },
+);
+
+export default mongoose.model('Post', PostSchema)
